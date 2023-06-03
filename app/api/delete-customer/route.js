7c8555c53defc0ecefc2e0a8af24a532b11d51db
johnkirtley <kirtleyj16@gmail.com>
2023-06-03 15:58:52 -0700

    posture fix

--- app/api/delete-customer/route.js
+++ app/api/delete-customer/route.js
@@ -1,18 +1,18 @@
 /* eslint-disable import/prefer-default-export */
 import Stripe from 'stripe';
 
-// let stripeKey;
+let stripeKey;
 
-// if (process.env.NEXT_PUBLIC_ENV === 'prod') {
-//     stripeKey = process.env.NEXT_PUBLIC_STRIPE_SK_PROD;
-// } else {
-//     stripeKey = process.env.NEXT_PUBLIC_STRIPE_SK;
-// }
+if (process.env.NEXT_PUBLIC_ENV === 'prod') {
+    stripeKey = process.env.NEXT_PUBLIC_STRIPE_SK_PROD;
+} else {
+    stripeKey = process.env.NEXT_PUBLIC_STRIPE_SK;
+}
 
 // const stripe = require('stripe')(stripeKey);
 
 export async function POST(req) {
-    const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SK);
+    const stripe = new Stripe(stripeKey);
     const { customerId } = await req.json();
 
     const customer = await stripe.customers.del(`${customerId}`);
--- app/api/get-customer/route.js
+++ app/api/get-customer/route.js
@@ -1,16 +1,16 @@
 /* eslint-disable import/prefer-default-export */
 import Stripe from 'stripe';
 
-// let stripeKey;
+let stripeKey;
 
-// if (process.env.NEXT_PUBLIC_ENV === 'prod') {
-//     stripeKey = process.env.NEXT_PUBLIC_STRIPE_SK_PROD;
-// } else {
-//     stripeKey = process.env.NEXT_PUBLIC_STRIPE_SK;
-// }
+if (process.env.NEXT_PUBLIC_ENV === 'prod') {
+    stripeKey = process.env.NEXT_PUBLIC_STRIPE_SK_PROD;
+} else {
+    stripeKey = process.env.NEXT_PUBLIC_STRIPE_SK;
+}
 
 export async function POST(req) {
-    const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SK);
+    const stripe = new Stripe(stripeKey);
 
     const { email } = await req.json();
 
