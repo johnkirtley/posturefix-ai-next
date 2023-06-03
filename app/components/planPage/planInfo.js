eb5a8439bf308466abfc46bf069f33c557a3f643
johnkirtley <kirtleyj16@gmail.com>
2023-06-03 16:18:24 -0700

    stripe product

--- app/components/planPage/planInfo.js
+++ app/components/planPage/planInfo.js
@@ -1,12 +1,4 @@
 const devPlanInfo = [
-    // {
-    //     name: 'Align',
-    //     type: 'Lifetime Deal',
-    //     features: [''],
-    //     price: 100,
-    //     savings: 0,
-    //     id: 'price_1NBrJ5IEkyn8v16oh61IkPJX',
-    // },
     {
         name: 'Align (Early Bird)',
         type: 'Yearly',
@@ -27,23 +19,21 @@ const devPlanInfo = [
 
 const prodPlanInfo = [
     {
-        name: 'bronze',
-        features: ['Order Request Portal', 'SMS Notifications', 'Unlimited QR Codes', 'Code Generation Tool'],
-        price: '$29',
-        id: 'price_1N6Qv2CHTaDRPPC8tAQZ4kcj',
+        name: 'Align (Early Bird)',
+        type: 'Yearly',
+        features: ['3 Levels of Progressive Exercise Difficulty', 'Unlimited Maintenance Phase Workout Generations', 'Random Stretch + Exercise Generator'],
+        price: 45,
+        savings: 25,
+        id: 'price_1NF3abIEkyn8v16omctzhUZj',
     },
     {
-        name: 'silver',
-        features: ['All Features From Bronze', 'Code Usage Analytics', 'Direct SMS With Customers'],
-        price: '$39',
-        id: 'price_1N6QuvCHTaDRPPC8bBUu23NZ',
+        name: 'Align (Early Bird)',
+        type: 'Monthly',
+        features: ['3 Levels of Progressive Exercise Difficulty', 'Unlimited Maintenance Phase Workout Generations', 'Random Stretch + Exercise Generator'],
+        price: 5,
+        savings: 0,
+        id: 'price_1NF3aWIEkyn8v16oCCl35osc',
     },
-    // {
-    //     name: 'gold',
-    //     features: ['All Features From Gold', 'Credit Based Billing', 'Label Printer'],
-    //     price: '$19.99',
-    //     id: 'price_1MmgzqGYpJAcieX9yPRbIvYY',
-    // },
 ];
 
 export { prodPlanInfo, devPlanInfo };
